0703e29729529b5bba440f58a960cbbf1dba370c
JohnnathanCastilloDuarte <jcastillo@cercal.cl>
2021-10-06 13:01:40 -0500

    Función terminada para identificar cuando se encuentre online / offline funcional

--- config.ini.php
+++ config.ini.php
@@ -6,23 +6,24 @@ include("smarty/libs/Smarty.class.php");
 $smarty = new smarty;
 
 $variable_url = $_SERVER['HTTP_HOST'];
-echo $variable_url;
-
-/*
+$db_username = "";
+$db_pass="";
 $db_server='localhost';
-//$db_username='root';
-//$db_pass='';
-//Variables para productivo
-$db_username='god_consultario';
-$db_pass='14ad7a591227b3733c6b9b56793bb10c';
+
+if($variable_url == "cercal.net"){
+	$db_username='god_consultario';
+	$db_pass='14ad7a591227b3733c6b9b56793bb10c';
+}else{
+	$db_username='root';
+	$db_pass='';
+}
 $db_database='god_CerNet_2_0';
 $connect = mysqli_connect($db_server,$db_username,$db_pass,$db_database);
-
 
 	if($connect === false)
 	{	
 		die("Error, no se puede establecer conexion a la base de datos ".mysqli_connect_error());
 	}
-mysqli_set_charset($connect,"utf8");*/
+mysqli_set_charset($connect,"utf8");
 
 ?>
