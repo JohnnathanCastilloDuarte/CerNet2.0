71e9c56dec54e2c3e15f8b43cbecd668fe784aaa
JohnnathanCastilloDuarte <jcastillo@cercal.cl>
2021-10-19 16:34:32 -0500

    cache de smarty

--- templates_c/c16cadc4633304385901fdf24a0af0514e0a0591_0.file.inicio_documentacion.tpl.php
+++ templates_c/c16cadc4633304385901fdf24a0af0514e0a0591_0.file.inicio_documentacion.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2021-10-12 22:37:20
+/* Smarty version 3.1.34-dev-7, created on 2021-10-19 23:26:19
   from 'C:\xampp\htdocs\CerNet2.0\templates\documentacion\inicio_documentacion.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_6165f2002c60a2_89577248',
+  'unifunc' => 'content_616f37fba11557_61224568',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c16cadc4633304385901fdf24a0af0514e0a0591' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CerNet2.0\\templates\\documentacion\\inicio_documentacion.tpl',
-      1 => 1634070919,
+      1 => 1634216392,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6165f2002c60a2_89577248 (Smarty_Internal_Template $_smarty_tpl) {
+function content_616f37fba11557_61224568 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div class="row">
   <div class="col-sm-12">
--- templates_c/dacc3226028ac407487abd6292caa5e047f333c3_0.file.administracion_documentacion.tpl.php
+++ templates_c/dacc3226028ac407487abd6292caa5e047f333c3_0.file.administracion_documentacion.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2021-10-11 17:43:00
+/* Smarty version 3.1.34-dev-7, created on 2021-10-19 23:26:33
   from 'C:\xampp\htdocs\CerNet2.0\templates\documentacion\head_templates\administracion_documentacion.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_61645b84ba4e81_64666795',
+  'unifunc' => 'content_616f38099f0ae2_28410665',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'dacc3226028ac407487abd6292caa5e047f333c3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CerNet2.0\\templates\\documentacion\\head_templates\\administracion_documentacion.tpl',
-      1 => 1633966970,
+      1 => 1634216392,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61645b84ba4e81_64666795 (Smarty_Internal_Template $_smarty_tpl) {
+function content_616f38099f0ae2_28410665 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="row">
   <div class="col-sm-12">
     <div class="card">
