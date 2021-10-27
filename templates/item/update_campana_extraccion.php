bfd8f277706e6494d870dd330b0d152319b8f003
JohnnathanCastilloDuarte <jcastillo@cercal.cl>
2021-10-27 11:48:47 -0500

    Backend creación item campana
    Se crea el backend funcional del item campana y se adjunta modelo de la tabla

--- templates/item/update_campana_extraccion.php
+++ templates/item/update_campana_extraccion.php
@@ -1,4 +1,30 @@
 <?php 
 
-$smarty->display("item/update_campana_extraccion.tpl");
- ?>
+
+$id_tipo = $_GET['type'];
+$id_item = $_GET['item'];
+
+
+echo "<input type='hidden' id='type_campana' value='".$id_tipo."'>";
+
+echo "<input type='hidden' id='id_item_campana' value='".$id_item."'>";
+
+$empresas = mysqli_prepare($connect,"SELECT id_empresa, nombre FROM empresa");
+mysqli_stmt_execute($empresas);
+mysqli_stmt_store_result($empresas);
+mysqli_stmt_bind_result($empresas, $id_empresa, $nombre_empresa);
+
+$array_empresa = array();
+
+while($row = mysqli_stmt_fetch($empresas)){
+
+$array_empresa[]=array(
+  'id_empresa'=>$id_empresa,
+  'nombre_empresa'=>$nombre_empresa	
+  );
+}
+
+$smarty->assign("array_empresa",$array_empresa);
+
+$smarty->display("item/update_campana_extraccion.tpl");    
+?>
--- templates_c/5a2460fc03c75e8363fd9c4ee083f5e44948f91e_0.file.update_campana_extraccion.tpl.php
+++ templates_c/5a2460fc03c75e8363fd9c4ee083f5e44948f91e_0.file.update_campana_extraccion.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2021-10-27 15:31:50
+/* Smarty version 3.1.34-dev-7, created on 2021-10-27 18:36:16
   from 'C:\xampp\htdocs\CerNet2.0\templates\item\update_campana_extraccion.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_617954c667f550_49538673',
+  'unifunc' => 'content_61798000ddd531_11476878',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5a2460fc03c75e8363fd9c4ee083f5e44948f91e' => 
     array (
       0 => 'C:\\xampp\\htdocs\\CerNet2.0\\templates\\item\\update_campana_extraccion.tpl',
-      1 => 1635341509,
+      1 => 1635352551,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_617954c667f550_49538673 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61798000ddd531_11476878 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="row">
   <div class="col-sm-12">
-    {}
     <div class="card">
       <div class="card-header">
-        <h5>
-          Edición del equipo <span>{}</span>
+        <h5 id="text_enunciado_campana">
+         
         </h5>
       </div>
       <div class="card-body">
@@ -43,17 +42,27 @@ function content_617954c667f550_49538673 (Smarty_Internal_Template $_smarty_tpl)
           <div id="step-12">
             <div class="form-row">
               <div class="col-sm-6">
-                <input type="hidden" id="id_item_ultrafreezer" value="$ultrafreezer.id_ultrafreezer}">
-                <input type="hidden" id="id_item_2_ultrafreezer" value="$ultrafreezer.id_item}">
                 <label>Nombre:</label>
-                <input type="text" id="nombre_campana" class="form-control" value="{}">
+                <input type="text" id="nombre_campana" class="form-control" required>
               </div>
               <div class="col-sm-6">
-                <label>Empresa</label>
-                <select class="form-control" id="empresa_ultrafreezer">
-                  <option value="0">Seleccione</option>
-
-                  <option value="{}">{}</option>
+                <label id="empresa_label">Empresa</label>
+                <select class="form-control" id="empresa_campana" required>
+                  <option value="<?php echo $_smarty_tpl->tpl_vars['bodega']->value['id_empresa'];?>
+"><?php echo $_smarty_tpl->tpl_vars['bodega']->value['nombre_empresa'];?>
+</option>
+                    <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['array_empresa']->value, 'empresa');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['empresa']->value) {
+?>
+                  <option value="<?php echo $_smarty_tpl->tpl_vars['empresa']->value['id_empresa'];?>
+"><?php echo $_smarty_tpl->tpl_vars['empresa']->value['nombre_empresa'];?>
+</option>
+                  <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                 </select>
               </div>
 
@@ -62,47 +71,48 @@ function content_617954c667f550_49538673 (Smarty_Internal_Template $_smarty_tpl)
             <div class="form-row">
               <div class="col-sm-6">
                 <label>Ubicación del equipo:</label>
-                <input type="text" id="fabricante_ultrafreezer" class="form-control" placeholder="ubicacion del equipo en el lugar empresa" value="{}">
+                <input type="text" id="ubicacion_campana" class="form-control" placeholder="ubicacion del equipo en el lugar empresa" >
               </div>
               <div class="col-sm-6">
                 <label>Dirección:</label>
-                <input type="text" id="direccion_campana" class="form-control" placeholder="Direccion campana" value="{}">
+                <input type="text" id="direccion_campana" class="form-control" placeholder="Direccion campana" >
               </div>
             </div>
             <br>
             <div class="form-row">
               <div class="col-sm-4">
                 <label>Tipo de campana:</label>
-                <input type="text" class="form-control" id="tipo_campana" value="{}">
+                <input type="text" class="form-control" id="tipo_campana" >
               </div>
               <div class="col-sm-4">
-                <label>Marca/fabricante:</label>
-                <input type="text" class="form-control" id="marca_campana" value="{}">
+                <label>Marca:</label>
+                <input type="text" class="form-control" id="marca_campana" >
               </div>
               <div class="col-sm-4">
                 <label>Modelo:</label>
-                <input type="text" class="form-control" id="modelo_campana" value="{}">
+                <input type="text" class="form-control" id="modelo_campana" >
               </div>
             </div>
             <br>
             <div class="form-row">
               <div class="col-sm-4">
                 <label>Codigo interno:</label>
-                <input type="text" id="codigo_interno_campana" class="form-control" placeholder="N° Serie" value="{}">
+                <input type="text" id="codigo_interno_campana" class="form-control" placeholder="Codigo interno" >
               </div>
               <div class="col-sm-4">
                 <label>Serie</label>
-                <input type="text" id="serie_campana" class="form-control" placeholder="Codigo interno" value="{}">
+                <input type="text" id="serie_campana" class="form-control" placeholder="N° Serie" >
               </div>
               <div class="col-sm-4">
                 <label>Año fabricación</label>
-                <input type="date" id="fecha_fabricacion_campana" class="form-control" placeholder="" value="{}">
+                <input type="date" id="fecha_fabricacion_campana" class="form-control" placeholder="" >
               </div>
             </div>
+            <br>
             <div class="form-row">
               <div class="col-sm-4">
-                <label>R.Velocidad de aire</label>
-                <input class="form-control" type="text" id="velocidad_aire" placeholder="velocidad aire" value="{}">
+                <label>Requisito velocidad de aire</label>
+                <input class="form-control" type="text" id="velocidad_aire_campana" placeholder="Velocidad aire" >
               </div>
             </div>
           </div>
@@ -111,15 +121,20 @@ function content_617954c667f550_49538673 (Smarty_Internal_Template $_smarty_tpl)
       </div>
       <!--Cierre del wizard-->
       <div class="divider"></div>
-      <center>
-       <button class="btn btn-outline-success">Enviar</button>
-     </center>
-   </div>
- </div>
- {}
+
+  
+  </div>
+  <div class="row" style="text-align:center;">
+    <div class="col-sm-12">
+      <button class="mb-2 mr-2  btn-shadow btn-outline-2x btn btn-outline-info" id="btn_editar_item_campana">Actualizar</button>
+      <button class="mb-2 mr-2  btn-shadow btn-outline-2x btn btn-outline-success" id="btn_nuevo_item_campana">Crear</button>
+    </div>
+  </div>
+
+  </div>
 </div>
 </div>
 <?php echo '<script'; ?>
- type="text/javascript" src="design/js/update_ultrafreezer.js"><?php echo '</script'; ?>
+ type="text/javascript" src="design/js/campana.js"><?php echo '</script'; ?>
 ><?php }
 }
