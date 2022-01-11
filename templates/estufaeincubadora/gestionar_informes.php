bb3dd9890aca5be9ba61352fe0e905c2b50104ee
LuisNovoa01 <92805860+LuisNovoa01@users.noreply.github.com>
2022-01-11 09:24:40 -0500

    modificación de gestor de informes incubadora

--- templates/estufaeincubadora/gestionar_informes.php
+++ templates/estufaeincubadora/gestionar_informes.php
@@ -52,7 +52,7 @@ mysqli_stmt_fetch($tipo_informe_2);
 
 $calificacion = mysqli_prepare($connect,"SELECT a.id_asignado, b.numot, f.nombre,  d.nombre,  e.nombre, e.apellido  
 	FROM item_asignado as a, numot as b, servicio as c, empresa as d, persona as e, item as f 
-	WHERE c.id_servicio_tipo =  $id_servicio_calificacion AND  c.id_numot = b.id_numot AND  a.id_item = f.id_item  AND b.id_empresa = d.id_empresa AND a.id_usuario = e.id_usuario ");
+	WHERE c.id_servicio_tipo =  $id_servicio_calificacion AND  c.id_numot = b.id_numot AND  a.id_item = f.id_item  AND b.id_empresa = d.id_empresa AND a.id_usuario = e.id_usuario AND f.id_tipo = 6 ");
 mysqli_stmt_execute($calificacion);
 mysqli_stmt_store_result($calificacion);
 mysqli_stmt_bind_result($calificacion, $id_asignado_c, $numot_c, $item_c, $empresa_c, $usuario_nombre_c, $usuario_apellido_c);
